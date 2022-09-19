4cbfe0aefe593d8501e9e6be2634af56ace45104
CodeZage <hectorthoegersen@gmail.com>
2022-09-19 14:40:01 +0200

    added files

---
+++ DotEnv.cs
@@ -0,0 +1,32 @@
+﻿namespace MedBot; 
+
+/// <summary>
+/// Class used to load environment variables from a file.
+/// </summary>
+public static class DotEnv
+{
+    /// <summary>
+    /// Loads environment variables from the specified file into the programs process.
+    /// </summary>
+    /// <param name="filePath">The path to the .env file.</param>
+    public static void LoadEnvironmentVariables(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("ERROR: No .env file found.");
+            return;
+        }
+
+        foreach (var line in File.ReadAllLines(filePath))
+        {
+            var parts = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(parts[0], parts[1]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,17 @@ public class Program
 
     public static Task Main(string[] args)
     {
+#if DEBUG // Loads the correct .env file depending on whether or not you are running in debug configuration or release
+        var path = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+        DotEnv.LoadEnvironmentVariables(Path.Combine(path!, ".env"));
+#else
+        DotEnv.LoadEnvironmentVariables(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
+#endif
+
+        var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+
+        Console.WriteLine("this is your token " + token);
+
         return new Program().MainAsync();
     }
 
@@ -20,25 +31,45 @@ public class Program
 
         _client.Log += Log;
         _client.Ready += ClientReady;
+        _client.SlashCommandExecuted += SlashCommandHandler;
 
-        var token = "ODEzMDQwNzQyNzU3OTU3NjUy.Gy_Nzp.TVGI6exLy3t7zZiLkLIeHH8GZrWxk09I4GqC7o";
-
-        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"));
         await _client.StartAsync();
 
         await Task.Delay(-1);
     }
 
+    #region Client Functions
+
     private async Task ClientReady()
     {
-        var guild = _client.GetGuild(813033894659620874);
+        var guild = _client.GetGuild(813033894659620874); // Defines the Medialogy discord server
+
+        List<ApplicationCommandProperties> applicationCommandProperties = new();
+
+        await _client.BulkOverwriteGlobalApplicationCommandsAsync(applicationCommandProperties.ToArray());
 
         var studentCommand =
             new SlashCommandBuilder().WithName("student").WithDescription("Declare yourself a student.");
 
+        var selectYearCommand = new SlashCommandBuilder().WithName("select-year")
+            .WithDescription("Select your current study year.").AddOption(new SlashCommandOptionBuilder()
+                .WithName("year")
+                .WithDescription("Your current study year.")
+                .WithRequired(true)
+                .AddChoice("1st Year", 1)
+                .AddChoice("2nd Year", 2)
+                .AddChoice("3rd Year", 3)
+                .AddChoice("4th Year", 4)
+                .AddChoice("5th Year", 5)
+                .WithType(ApplicationCommandOptionType.Integer)
+            );
+
+        var commands = new List<SlashCommandBuilder> { studentCommand, selectYearCommand };
+        
         try
         {
-            await guild.CreateApplicationCommandAsync(studentCommand.Build());
+            foreach (var command in commands) await guild.CreateApplicationCommandAsync(command.Build());
         }
         catch (HttpException exception)
         {
@@ -53,6 +84,7 @@ public class Program
         return Task.CompletedTask;
     }
 
+    #endregion
 
     #region Command Handling
 
@@ -63,7 +95,35 @@ public class Program
             case "student":
                 await StudentCommandHandler(command);
                 break;
+
+            case "select-year":
+                await SelectYearCommandHandler(command);
+                break;
         }
+    }
+
+    private async Task SelectYearCommandHandler(SocketSlashCommand command)
+    {
+        switch ((long)command.Data.Options.First().Value)
+        {
+            case 1:
+                await (command.User as IGuildUser).AddRoleAsync(813118674642403343);
+                break;
+            case 2:
+                await (command.User as IGuildUser).AddRoleAsync(813124643799302146);
+                break;
+            case 3:
+                await (command.User as IGuildUser).AddRoleAsync(813124991641845771);
+                break;
+            case 4:
+                await (command.User as IGuildUser).AddRoleAsync(813127288836849694);
+                break;
+            case 5:
+                await (command.User as IGuildUser).AddRoleAsync(813131657791012884);
+                break;
+        }
+
+        await command.RespondAsync($"You are now a {command.Data.Options.First().Name} student.");
     }
 
     private async Task StudentCommandHandler(SocketSlashCommand command)
